3c5359fcef93d497783ce695f150a8cdf8964c20
The-Bearded-Vegan <63019033+The-Bearded-Vegan@users.noreply.github.com>
2020-04-02 08:11:38 +0100

    Initial Commit

---
+++ cin and strings/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cin and strings
+//
+//  Created by Richard Cheesman on 02/04/2020.
+//  Copyright © 2020 Richard Cheesman. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
